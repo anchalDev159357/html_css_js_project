fffcb18fca2c2059b3df3d681d49e9a1b4ee39ac
anchalDev159357 <singhanchal159357@gmail.com>
2023-08-22 16:44:08 +0530

    added changes

--- project 4/script.js
+++ project 4/script.js
@@ -1,11 +1,20 @@
-const models = document.querySelectorAll(".btn");
-const contents = document.querySelectorAll(".content");
-const closeBtn = document.getElementsByClassName(".close-btn");
-const Container = document.getElementsByClassName(".container");
+const buttons = document.querySelectorAll(".btn");
+const contents = document.getElementsByClassName("content");
+const closeBtn = document.getElementsByClassName("close-btn");
+const Container = document.getElementsByClassName("container");
 
-for (let i = 0; i < models.length; i++) {
-  models[i].addEventListener("click", function () {
-    console.log('button clicked')
-    
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    console.log("button clicked");
+    if (contents) {
+      contents[i].classList.remove("hidden");
+      for (let j = 0; j < contents.length; j++) {
+        if (j !== i) {
+          contents[j].classList.add("hidden");
+        }
+      }
+    } else {
+      console.log("there is nothing");
+    }
   });
 }
