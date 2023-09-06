d8b97f1cdf927b349bc980ad0699540fa46807d6
anchalDev159357 <singhanchal159357@gmail.com>
2023-09-06 13:51:20 +0530

    added changes

--- project 14/script.js
+++ project 14/script.js
@@ -39,16 +39,19 @@ canvas.addEventListener("mousemove", (e) => {
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2, true);
-  ctx.fillstyle = color;
+  ctx.fillStyle = color;
+  ctx.lineWidth = size;
   ctx.fill();
+  // ctx.strokeStyle = color;
+  // ctx.stroke();
 }
 
 function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
-  ctx.strokeStyle = color;
   ctx.lineWidth = size * 2;
+  ctx.strokeStyle = color;
   ctx.stroke();
 }
 function updateSizeOnScreen() {
