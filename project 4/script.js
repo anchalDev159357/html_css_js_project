840e8a80849cdbeed6798a8012dba7e7684c1ea8
anchalDev159357 <singhanchal159357@gmail.com>
2023-09-10 21:19:12 +0530

    added changes

--- project 4/script.js
+++ project 4/script.js
@@ -1,20 +1,50 @@
-const buttons = document.querySelectorAll(".btn");
-const contents = document.getElementsByClassName("content");
+const htmlContent = document.getElementById("html");
+const cssContent = document.getElementById("css");
+const jsContent = document.getElementById("js");
+const htmlCloseBtn = document.getElementById("html-close-btn");
+const cssCloseBtn = document.getElementById("css-close-btn");
+const jsCloseBtn = document.getElementById("js-close-btn");
 const closeBtn = document.getElementsByClassName("close-btn");
 const Container = document.getElementsByClassName("container");
+const htmlBtn = document.getElementById("msg1");
+const cssBtn = document.getElementById("msg2");
+const jsBtn = document.getElementById("msg3");
+const welcome = document.querySelector('h1')
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", () => {
-    console.log("button clicked");
-    if (contents) {
-      contents[i].classList.remove("hidden");
-      for (let j = 0; j < contents.length; j++) {
-        if (j !== i) {
-          contents[j].classList.add("hidden");
-        }
-      }
-    } else {
-      console.log("there is nothing");
-    }
-  });
-}
+htmlBtn.addEventListener("click", () => {
+  welcome.style.opacity = 0
+  htmlContent.classList.remove("hidden");
+  cssContent.classList.add("hidden");
+  jsContent.classList.add("hidden");
+});
+cssBtn.addEventListener("click", () => {
+  welcome.style.opacity = 0
+  htmlContent.classList.add("hidden");
+  cssContent.classList.remove("hidden");
+  jsContent.classList.add("hidden");
+});
+jsBtn.addEventListener("click", () => {
+  welcome.style.opacity = 0
+  htmlContent.classList.add("hidden");
+  cssContent.classList.add("hidden");
+  jsContent.classList.remove("hidden");
+});
+
+htmlCloseBtn.addEventListener("click", () => {
+  welcome.style.opacity = 1
+  htmlContent.classList.add("hidden");
+  cssContent.classList.add("hidden");
+  jsContent.classList.add("hidden");
+});
+cssCloseBtn.addEventListener("click", () => {
+  welcome.style.opacity = 1
+  htmlContent.classList.add("hidden");
+  cssContent.classList.add("hidden");
+  jsContent.classList.add("hidden");
+});
+jsCloseBtn.addEventListener("click", () => {
+  welcome.style.opacity = 1
+  htmlContent.classList.add("hidden");
+  cssContent.classList.add("hidden");
+  jsContent.classList.add("hidden");
+});
