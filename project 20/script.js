07cdd2c8947c1e7f5ccee9b4090b98f5a573d41a
anchalDev159357 <singhanchal159357@gmail.com>
2023-09-12 08:41:08 +0530

    added changes

---
+++ project 20/script.js
@@ -0,0 +1,36 @@
+const container = document.getElementById("container");
+const colors = [
+  "#2626e1",
+  "#ff0000",
+  "#16c116",
+  "#ffff00",
+  "#800080",
+  "#20b2aa",
+  '#fff',
+];
+const squares = 500;
+
+for (let i = 0; i < squares; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseout", () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+function removeColor(element) {
+  element.style.background = " #1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
