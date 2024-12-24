b607d0e36d3c30efdd937c6f9c0187999adf417a
anchalDev159357 <singhanchal159357@gmail.com>
2024-12-24 09:33:34 +0530

    add

--- blurry loading page/script.js
+++ blurry loading page/script.js
@@ -9,6 +9,7 @@ function blurring() {
   if (load > 99) {
     clearInterval(int);
   }
+  
   loading.innerText = `${load}%`;
   loading.style.opacity = scale(load, 0, 100, 1, 0);
   bg.style.filter = `blur(${scale(load , 0 ,100, 30 , 0)}px)`
